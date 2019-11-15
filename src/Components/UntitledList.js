20ae6417a57de826fa9c41e77c1a05da3a3416ae
bblais23 <ben.blais6@gmail.com>
2019-11-15 14:07:18 -0500

    Add css for list

--- src/Components/UntitledList.js
+++ src/Components/UntitledList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import './UntitledList.css';
+
 export default function UntitledList() {
 	return <main className="listPaper">
-		<h1>to do:</h1>
+		<section class="listContent">
+			<h1>to do:</h1>
+		</section>
 	</main>
 }
