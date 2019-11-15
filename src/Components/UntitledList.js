b105d4f59bf82da21e288b52a94ebf5c4ce0b104
bblais23 <ben.blais6@gmail.com>
2019-11-15 16:08:42 -0500

    Don't need 'listContent'

--- src/Components/UntitledList.js
+++ src/Components/UntitledList.js
@@ -4,13 +4,11 @@ import './UntitledList.css';
 
 export default function UntitledList() {
 	return <main className="listPaper">
-		<section class="listContent">
-			<h1>to do:</h1>
-			<p>A list item</p>
-			<p>A list item</p>
-			<p>A list item</p>
-			<p>A list item</p>
-			<p>A list item</p>
-		</section>
+		<h1>to do:</h1>
+		<p>A list item</p>
+		<p>A list item</p>
+		<p>A list item</p>
+		<p>A list item</p>
+		<p>A list item</p>
 	</main>
 }
