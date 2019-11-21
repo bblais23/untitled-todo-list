6a2bdc1921e2b654f3ed08cbacdc04cbe4d0eabf
bblais23 <ben.blais6@gmail.com>
2019-11-20 19:11:27 -0500

    change title handler

--- src/Components/UntitledItem.js
+++ src/Components/UntitledItem.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 import './UntitledItem.css';
 
-export default function UntitledItem({children, checked, enabled, setChecked}) {
+export default function UntitledItem({children, checked, enabled, setChecked, setEnabled, setTitle}) {
 
 	const textInput = enabled ? 
-		<input className="todo-text" type="text" value={children}></input> :
+		<input 
+			className="todo-text" 
+			type="text" 
+			value={children} 
+			onChange={e => setTitle(e.target.value)}
+		/> :
 		<p className="todo-text">{children}</p>;
 
 	return <label className="untitled-item">
 		<input 
 			type="checkbox" 
 			onChange={e => setChecked(e.target.checked)} 
-			checked={checked} 
+			checked={checked}
 		/>
 		{textInput}
 	</label>;
