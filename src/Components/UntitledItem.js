da209a8df9cbdab3c794c01a18411ae3dff38c02
bblais23 <ben.blais6@gmail.com>
2019-11-20 19:00:31 -0500

    Update items when checked

--- src/Components/UntitledItem.js
+++ src/Components/UntitledItem.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import './UntitledItem.css';
 
-export default function UntitledItem({children, checked, enabled}) {
+export default function UntitledItem({children, checked, enabled, setChecked}) {
 
 	const textInput = enabled ? 
 		<input className="todo-text" type="text" value={children}></input> :
 		<p className="todo-text">{children}</p>;
 
 	return <label className="untitled-item">
-		<input type="checkbox" checked={checked}></input>
+		<input 
+			type="checkbox" 
+			onChange={e => setChecked(e.target.checked)} 
+			checked={checked} 
+		/>
 		{textInput}
 	</label>;
 }
--- src/Components/UntitledList.js
+++ src/Components/UntitledList.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Item from './UntitledItem';
 
 import './UntitledList.css';
 
+const todoItemsDefault = [
+	{
+		item: 'create todo list',
+		checked: true,
+	},
+	{
+		item: 'store items locally',
+		checked: false,
+	},
+	{
+		item: 'list in prod',
+		checked: false,
+	},
+]
+
 export default function UntitledList() {
+
+	const [todoItems, setTodoItems] = useState(todoItemsDefault);
+
+	const updateOneItem = (updateItem, updateIndex) => 
+		setTodoItems(
+			todoItems.map(
+				(item, index) => 
+					index === updateIndex ? updateItem : item));
+
+	const items = todoItems.map( 
+		({item, checked}, index) => 
+			<Item
+				setChecked={checked => updateOneItem({item, checked}, index)}
+				key={index} 
+				checked={checked}>
+				{item}
+			</Item>);
+
 	return <main className="listPaper">
 		<h1>to do:</h1>
-		<Item>A list Item</Item>
-	</main>
+		{items}
+	</main>;
 }
